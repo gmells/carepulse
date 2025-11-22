35b3206e7f9c462e471feeeddb0ccd54a340dea1
gmells <victormells30@gmail.com>
2025-11-22 23:51:20 +0000

    add CustomFormField to PatientForm component

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import CustomFormField from "../CustomFormField";
+import CustomFormField from "@/components/CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
